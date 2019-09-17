4bb41f653aa458414f34397c298f03ef89e75bad
zyh <Zhaoyh_main@163.com>
2019-09-17 14:24:46 +0800

    react-app

---
+++ build/precache-manifest.a076b2a0c16e95ff8130fa0b64ffcbe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d954c1d95eb0a9b6f75d2fccbc37bc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b06bdb362c4c9c0dbfe",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4d98c46e5ff0c9569a5b",
+    "url": "/static/js/2.0fd5c805.chunk.js"
+  },
+  {
+    "revision": "4b06bdb362c4c9c0dbfe",
+    "url": "/static/js/main.a67cebc9.chunk.js"
+  },
+  {
+    "revision": "3b088da4d0638e70ec55",
+    "url": "/static/js/runtime~main.f8f10a3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
